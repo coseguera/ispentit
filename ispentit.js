e6663e2418c856cdb2e9cad436b1339bc72e8bf5
coseguera <coseguera@users.noreply.github.com>
2014-02-09 23:17:06 -0800

    http server added on port 8000

--- ispentit.js
+++ ispentit.js
@@ -1,1 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, resp) {
+    console.log('request received');
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    response.end('ispentit received your request');
+}).listen(8000);
+
 console.log('ispentit is ready');

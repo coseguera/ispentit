12fae79034c505026d24c4d21f785ae60ca684ea
coseguera <coseguera@users.noreply.github.com>
2014-02-09 23:19:28 -0800

    fixed response is not defined

--- ispentit.js
+++ ispentit.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, resp) {
     console.log('request received');
-    response.writeHead(200, { 'content-type': 'text/plain' });
-    response.end('ispentit received your request');
+    resp.writeHead(200, { 'content-type': 'text/plain' });
+    resp.end('ispentit received your request');
 }).listen(8000);
 
 console.log('ispentit is ready');

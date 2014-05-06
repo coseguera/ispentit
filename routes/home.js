6928ccd4b942802563de223934a105516bf434db
coseguera <coseguera@users.noreply.github.com>
2014-05-06 02:23:13 +0000

    active func in navbar

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
 exports.setup = function(app) {
     app.get('/', function (req, res) {
-        res.render('index', { title: 'iSpentIt Home', message: 'Welcome to the site!' });
+        res.render('index', { title: 'iSpentIt Home', message: 'Welcome to the site!', page: { home: true }});
     })
     .get('/about', function (req, res) {
-        res.render('index', { title: 'iSpentIt About', message: 'About iSpentIt' });
+        res.render('index', { title: 'iSpentIt About', message: 'About iSpentIt', page: { about: true } });
     });
 };

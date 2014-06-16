5c380c2aa69f506709570bb2f6a70e97864ef254
coseguera <coseguera@users.noreply.github.com>
2014-06-16 01:11:31 +0000

    mongo transaction model di

--- models/mongo/transactionModel.js
+++ models/mongo/transactionModel.js
@@ -1,10 +1,11 @@
-var mongoose = require('mongoose'),
-    transactionSchema = mongoose.Schema({
-	date: Date,
-	concept: String,
-	amount: Number,
-	account: String,
-	person: String,
-	splitId: String
+exports.Transaction = function (mongoose) {
+    var transactionSchema = mongoose.Schema({
+        date: Date,
+        concept: String,
+        amount: Number,
+        account: String,
+        person: String,
+        splitId: String
     });
-exports.Transaction = mongoose.model('Transaction', transactionSchema);
+    return mongoose.model('Transaction', transactionSchema);
+};
